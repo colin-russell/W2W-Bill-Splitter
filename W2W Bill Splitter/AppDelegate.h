ac87a1e9fd51857bfb304cd9b84757b79328eecd
Colin Russell <colinrussell101@gmail.com>
2018-04-21 16:34:09 -0400

    Initial Commit

---
+++ W2W Bill Splitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  W2W Bill Splitter
+//
+//  Created by Colin on 2018-04-21.
+//  Copyright © 2018 Colin Russell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
