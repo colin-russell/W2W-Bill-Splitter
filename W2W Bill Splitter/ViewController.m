fd6f606d78e725a66afc3fc758d299399962af6c
Colin Russell <colinrussell101@gmail.com>
2018-04-22 12:30:28 -0400

    finished except stretch goals

--- W2W Bill Splitter/ViewController.m
+++ W2W Bill Splitter/ViewController.m
@@ -9,6 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UISlider *numOfPeopleSlider;
+@property (weak, nonatomic) IBOutlet UILabel *splitAmountLabel;
+@property (weak, nonatomic) IBOutlet UILabel *numOfPeopleLabel;
+@property (nonatomic) NSInteger numOfPeople;
 
 @end
 
@@ -17,12 +22,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.numOfPeople = 2;
+}
+- (IBAction)numOfPeopleSliderValueChanged:(UISlider *)sender {
+    self.numOfPeople = (int)sender.value;
+    self.numOfPeopleLabel.text = [NSString stringWithFormat:@"# of People: %lu", self.numOfPeople];
 }
 
+- (IBAction)calculateButtonPressed:(UIButton *)sender {
+    float amountPerPerson = [self.billAmountTextField.text floatValue]/self.numOfPeople;
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    self.splitAmountLabel.text = [NSString stringWithFormat:@"$%0.2f", amountPerPerson];
 }
 
 
